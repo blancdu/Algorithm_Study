486dc156f6219810c8de14436647f377f0d77322
Sejin Hong <hongsj36@gmail.com>
2022-04-26 02:00:58 +0900

    BOJ-1019 '책 페이지' [ 22/04/26 02:00:58 ] by Alub

---
+++ BOJ/1019/1019_1.py
@@ -0,0 +1,21 @@
+'''
+ 메모리: 113112 KB, 시간: 108 ms
+ 풀이 시간: 00:00:00
+ 2022.04.26
+ by Alub
+'''
+N = int(input())
+q = N
+expo = 0
+answer = [0] * 10
+while q >= 1:
+    q, mod = q // 10, q % 10
+    for i in range(10):
+        answer[i] += q * (10**expo)
+        if i < mod:
+            answer[i] += (10**expo)
+        elif i == mod:
+            answer[i] += (N % (10**expo)) + 1
+    expo += 1
+answer[0] -= int('1'*expo)
+print(*answer)
